125887778678ee89477872a703bbe434b90d4188
Tushar Mankar <tusharmankar02@gmail.com>
2024-10-08 17:00:49 +0530

    Create model of products

--- server/products/models.py
+++ server/products/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Products(models.Model):
+    images = models.ImageField( upload_to='upload/images')
+    name = models.CharField( max_length=150 , null=False ,blank=False)
+    price = models.DecimalField( max_digits=7, decimal_places=2 , null=False , blank=False)
+    description = models.TextField()
+    category = models.CharField( max_length=50 , null=False ,blank=False)
+    
+    
+    def __str__(self) -> str:
+        return self.name

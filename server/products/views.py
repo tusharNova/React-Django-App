b2fe1a098d17fcc70588325d87b5cb5f9a576e0e
Tushar Mankar <tusharmankar02@gmail.com>
2024-10-08 17:36:41 +0530

    add images in medai

--- server/products/views.py
+++ server/products/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Products
+from rest_framework import viewsets
+from .serializer import ProductSerializer
 
-# Create your views here.
+class ProductView(viewsets.ModelViewSet):
+    queryset = Products.objects.all()
+    serializer_class = ProductSerializer
+
+

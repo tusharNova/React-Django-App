79f5fed7540f33aa41ac86530e4cb297ec91ad62
Tushar Mankar <tusharmankar02@gmail.com>
2024-10-09 11:09:09 +0530

    Create navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import AddProducts from './components/AddProducts';
 // Bootstrap CSS
@@ -8,13 +8,15 @@ import "bootstrap/dist/js/bootstrap.bundle.min";
 import ProductDetails from './components/ProductDetail';
 import ShowProduct from './components/ShowProducts';
 import UpdateProduct from './components/UpdateProducts';
+import NavBarMenu from './components/NavBarMenu';
 function App() {
   return (
     <div className="App">
-      <AddProducts/>
+      <NavBarMenu/>
+      {/* <AddProducts/>
       <ProductDetails/>
       <ShowProduct/>
-      <UpdateProduct/>
+      <UpdateProduct/> */}
     </div>
   );
 }

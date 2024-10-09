2b5209b58134db276facaed4787cb229af757d7d
Tushar Mankar <tusharmankar02@gmail.com>
2024-10-09 15:59:18 +0530

    Show at Products pages

--- client/src/components/ShowProducts.jsx
+++ client/src/components/ShowProducts.jsx
@@ -1,21 +1,36 @@
-import React , {useState , useEffect} from 'react'
-import axios from 'axios'
- const ShowProduct = () => {
-    const [ products , setProducts] = useState([])
-    const getProducts = async () => {
-        const response = await axios.get('http://127.0.0.1:8000/api/');
-        console.log(response.data);
-    }
- 
-    useEffect(() =>{
-        getProducts();
-    } , []);
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
 
+const ShowProduct = () => {
+  const [products, setProducts] = useState([]);
+  const getProducts = async () => {
+    const response = await axios.get("http://127.0.0.1:8000/api/");
+    console.log(response.data);
+    setProducts(response.data);
+  };
 
+  useEffect(() => {
+    getProducts();
+  }, []);
 
-    return (
-        <div className="h2 display-3">Show Poduct </div>
-    )
-}
+  return (
+    <div className="product-item-show">
+      {products.map((product, index) => (
+        <Card className="m-2 rounded shadow-lg" style={{ width: "22rem" }}>
+          <Card.Img variant="top" src={product.images} />
+          <Card.Body>
+            <Card.Title>{product.name}</Card.Title>
+            <Card.Text>{product.price} </Card.Text>
+            <Card.Text>{product.description} </Card.Text>
+            <Card.Text>{product.category} </Card.Text>
+            <Button variant="primary">Show Details</Button>
+          </Card.Body>
+        </Card>
+      ))}
+    </div>
+  );
+};
 
 export default ShowProduct;

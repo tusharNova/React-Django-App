2e3fba1e61cf99b6b814eca85e04cd9fdd94f4a4
Tushar Mankar <tusharmankar02@gmail.com>
2024-10-25 18:14:49 +0530

    remove of server dueto error

--- server/README.md
+++
@@ -1,2 +0,0 @@
-
-# Code For Backend in Django
--- server/products/serializer.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.serializers import ModelSerializer
-
-from .models import Products
-
-
-class ProductSerializer(ModelSerializer):
-    class Meta:
-        model = Products
-        fields = '__all__'
